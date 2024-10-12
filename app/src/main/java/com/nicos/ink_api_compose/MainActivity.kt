d00907c146315b5bd38ce1b048005780f7b6c9be
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-10-12 23:08:51 +0300

    option to change the color for drawer and erase the drawer

--- app/src/main/java/com/nicos/ink_api_compose/MainActivity.kt
+++ app/src/main/java/com/nicos/ink_api_compose/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.nicos.ink_api_compose
 
 import android.annotation.SuppressLint
+import android.graphics.Matrix
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
@@ -8,51 +9,125 @@ import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.UiThread
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.Snapshot
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.ink.authoring.InProgressStrokeId
+import androidx.ink.authoring.InProgressStrokesFinishedListener
 import androidx.ink.authoring.InProgressStrokesView
 import androidx.ink.brush.Brush
 import androidx.ink.brush.StockBrushes
 import androidx.ink.brush.color.Color
 import androidx.ink.brush.color.toArgb
+import androidx.ink.geometry.ImmutableBox
+import androidx.ink.geometry.Vec
+import androidx.ink.rendering.android.canvas.CanvasStrokeRenderer
+import androidx.ink.strokes.Stroke
 import androidx.input.motionprediction.MotionEventPredictor
+import com.nicos.ink_api_compose.ui.theme.Blue
+import com.nicos.ink_api_compose.ui.theme.Green
 import com.nicos.ink_api_compose.ui.theme.Ink_Api_ComposeTheme
+import com.nicos.ink_api_compose.ui.theme.Pink
+import com.nicos.ink_api_compose.ui.theme.Red
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), InProgressStrokesFinishedListener {
     private lateinit var inProgressStrokesView: InProgressStrokesView
+    private val finishedStrokesState = mutableStateOf(emptySet<Stroke>())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         inProgressStrokesView = InProgressStrokesView(this)
+        inProgressStrokesView.addFinishedStrokesListener(this)
         enableEdgeToEdge()
         setContent {
             Ink_Api_ComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    DrawingSurface(inProgressStrokesView = inProgressStrokesView)
+                    val eraserBox = ImmutableBox.fromCenterAndDimensions(
+                        Vec.ORIGIN,
+                        Float.MAX_VALUE,
+                        Float.MAX_VALUE
+                    )
+                    DrawingSurface(
+                        innerPadding = innerPadding,
+                        inProgressStrokesView = inProgressStrokesView,
+                        finishedStrokesState = finishedStrokesState,
+                        eraseDrawer = {
+                            eraseWholeStrokes(
+                                eraserBox = eraserBox,
+                                finishedStrokesState = finishedStrokesState
+                            )
+                        }
+                    )
                 }
+            }
+        }
+    }
+
+    @UiThread
+    override fun onStrokesFinished(strokes: Map<InProgressStrokeId, Stroke>) {
+        finishedStrokesState.value += strokes.values
+        inProgressStrokesView.removeFinishedStrokes(strokes.keys)
+    }
+
+    private fun eraseWholeStrokes(
+        eraserBox: ImmutableBox,
+        finishedStrokesState: MutableState<Set<Stroke>>,
+    ) {
+        val threshold = 0.1f
+
+        val strokesToErase = finishedStrokesState.value.filter { stroke ->
+            stroke.shape.computeCoverageIsGreaterThan(
+                box = eraserBox,
+                coverageThreshold = threshold,
+            )
+        }
+        if (strokesToErase.isNotEmpty()) {
+            Snapshot.withMutableSnapshot {
+                finishedStrokesState.value -= strokesToErase
             }
         }
     }
 }
 
-@SuppressLint("ClickableViewAccessibility")
+@SuppressLint("ClickableViewAccessibility", "RestrictedApi")
 @Composable
 fun DrawingSurface(
-    inProgressStrokesView: InProgressStrokesView
+    innerPadding: PaddingValues,
+    inProgressStrokesView: InProgressStrokesView,
+    finishedStrokesState: MutableState<Set<Stroke>>,
+    eraseDrawer: () -> Unit,
 ) {
+    val canvasStrokeRenderer = CanvasStrokeRenderer.create()
     val currentPointerId = remember { mutableStateOf<Int?>(null) }
     val currentStrokeId = remember { mutableStateOf<InProgressStrokeId?>(null) }
-    val defaultBrush = Brush.createWithColorIntArgb(
+    var defaultBrush = Brush.createWithColorIntArgb(
         family = StockBrushes.pressurePenLatest,
         colorIntArgb = Color.Red.toArgb(),
-        size = 5F,
+        size = 15F,
         epsilon = 0.1F
     )
 
@@ -142,8 +217,73 @@ fun DrawingSurface(
                 rootView.addView(inProgressStrokesView)
                 rootView
             },
-        ) {
+        )
+        Canvas(modifier = Modifier) {
+            val canvasTransform = Matrix()
+            drawContext.canvas.nativeCanvas.concat(canvasTransform)
+            val canvas = drawContext.canvas.nativeCanvas
 
+            finishedStrokesState.value.forEach { stroke ->
+                canvasStrokeRenderer.draw(
+                    stroke = stroke,
+                    canvas = canvas,
+                    strokeToScreenTransform = canvasTransform
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .height(height = 100.dp)
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth(fraction = 0.7f)
+                .safeDrawingPadding(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            EraseDrawer(eraseDrawer = eraseDrawer)
+            SelectedColor(color = Red) {
+                defaultBrush =
+                    defaultBrush.copyWithColorIntArgb(colorIntArgb = Color(Color.Red.toArgb()).toArgb())
+            }
+            SelectedColor(color = Blue) {
+                defaultBrush =
+                    defaultBrush.copyWithColorIntArgb(colorIntArgb = Color(Color.Blue.toArgb()).toArgb())
+            }
+            SelectedColor(color = Pink) {
+                defaultBrush =
+                    defaultBrush.copyWithColorIntArgb(colorIntArgb = Color(Pink.value).toArgb())
+            }
+            SelectedColor(color = Green) {
+                defaultBrush =
+                    defaultBrush.copyWithColorIntArgb(colorIntArgb = Color.Green.toArgb())
+            }
         }
     }
+}
+
+@Composable
+private fun EraseDrawer(eraseDrawer: () -> Unit) {
+    Button(
+        onClick = {
+            eraseDrawer()
+        },
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_delete_forever_24),
+            contentDescription = "Delete",
+        )
+    }
+}
+
+@Composable
+private fun SelectedColor(
+    color: androidx.compose.ui.graphics.Color,
+    selectingColor: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .size(height = 70.dp, width = 50.dp)
+            .background(color = color)
+            .clickable {
+                selectingColor()
+            })
 }
