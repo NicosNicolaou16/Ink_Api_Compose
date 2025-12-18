e0c4ab199d1cccfb810c5149c40f76196e94bf41
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-12-18 19:56:52 +0200

    some clean up the code and update README file

--- app/src/main/java/com/nicos/ink_api_compose/data/stroke_converter/SerializedStrokeInputBatch.kt
+++
@@ -1,8 +0,0 @@
-package com.nicos.ink_api_compose.data.stroke_converter
-
-
-data class SerializedStrokeInputBatch(
-    val toolType: SerializedToolType,
-    val strokeUnitLengthCm: Float,
-    val inputs: List<SerializedStrokeInput>
-)
--- app/src/main/java/com/nicos/ink_api_compose/data/stroke_converter/StrokeConverter.kt
+++ app/src/main/java/com/nicos/ink_api_compose/data/stroke_converter/StrokeConverter.kt
@@ -1,13 +1,10 @@
 package com.nicos.ink_api_compose.data.stroke_converter
 
 import androidx.ink.brush.Brush
-import androidx.ink.brush.InputToolType
 import androidx.ink.brush.StockBrushes
 import androidx.ink.storage.decode
 import androidx.ink.storage.encode
-import androidx.ink.strokes.MutableStrokeInputBatch
 import androidx.ink.strokes.Stroke
-import androidx.ink.strokes.StrokeInput
 import androidx.ink.strokes.StrokeInputBatch
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
@@ -40,46 +37,6 @@ class StrokeConverters {
         )
     }
 
-    private fun serializeStrokeInputBatch(inputs: StrokeInputBatch): SerializedStrokeInputBatch {
-        val serializedInputs = mutableListOf<SerializedStrokeInput>()
-        val scratchInput = StrokeInput()
-
-        for (i in 0 until inputs.size) {
-            inputs.populate(i, scratchInput)
-            serializedInputs.add(
-                SerializedStrokeInput(
-                    x = scratchInput.x,
-                    y = scratchInput.y,
-                    timeMillis = scratchInput.elapsedTimeMillis.toFloat(),
-                    pressure = scratchInput.pressure,
-                    tiltRadians = scratchInput.tiltRadians,
-                    orientationRadians = scratchInput.orientationRadians,
-                    strokeUnitLengthCm = scratchInput.strokeUnitLengthCm,
-                )
-            )
-        }
-
-        val toolType =
-            when (inputs.getToolType()) {
-                InputToolType.STYLUS -> SerializedToolType.STYLUS
-                InputToolType.TOUCH -> SerializedToolType.TOUCH
-                InputToolType.MOUSE -> SerializedToolType.MOUSE
-                else -> SerializedToolType.UNKNOWN
-            }
-
-        return SerializedStrokeInputBatch(
-            toolType = toolType,
-            strokeUnitLengthCm = inputs.getStrokeUnitLengthCm(),
-            inputs = serializedInputs,
-        )
-    }
-
-    private fun deserializeStroke(serializedStroke: SerializedStroke): Stroke? {
-        val inputs = deserializeStrokeInputBatch(serializedStroke.inputs) ?: return null
-        val brush = deserializeBrush(serializedStroke.brush) ?: return null
-        return Stroke(brush = brush, inputs = inputs)
-    }
-
     private fun deserializeBrush(serializedBrush: SerializedBrush): Brush {
         val stockBrushFamily = enumToStockBrush[serializedBrush.stockBrush] ?: StockBrushes.marker()
 
@@ -89,34 +46,6 @@ class StrokeConverters {
             size = serializedBrush.size,
             epsilon = serializedBrush.epsilon,
         )
-    }
-
-    private fun deserializeStrokeInputBatch(
-        serializedBatch: SerializedStrokeInputBatch
-    ): StrokeInputBatch {
-        val toolType = InputToolType.TOUCH
-        when (serializedBatch.toolType) {
-            SerializedToolType.STYLUS -> InputToolType.STYLUS
-            SerializedToolType.TOUCH -> InputToolType.TOUCH
-            SerializedToolType.MOUSE -> InputToolType.MOUSE
-            else -> InputToolType.UNKNOWN
-        }
-
-        val batch = MutableStrokeInputBatch()
-
-        serializedBatch.inputs.forEach { input ->
-            batch.add(
-                type = toolType,
-                x = input.x,
-                y = input.y,
-                elapsedTimeMillis = input.timeMillis.toLong(),
-                pressure = input.pressure,
-                tiltRadians = input.tiltRadians,
-                orientationRadians = input.orientationRadians,
-            )
-        }
-
-        return batch
     }
 
     fun serializeStrokeToEntity(stroke: Stroke): StrokeEntity {
@@ -152,15 +81,5 @@ class StrokeConverters {
         val brush = deserializeBrush(serializedBrush)
 
         return Stroke(brush = brush, inputs = inputsStroke)
-    }
-
-    fun brushToString(brush: Brush): String {
-        val serializedBrush = serializeBrush(brush)
-        return gson.toJson(serializedBrush)
-    }
-
-    fun stringToBrush(jsonString: String): Brush {
-        val serializedBrush = gson.fromJson(jsonString, SerializedBrush::class.java)
-        return deserializeBrush(serializedBrush)
     }
 }
