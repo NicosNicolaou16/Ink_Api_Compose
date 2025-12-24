8c49072dc913f0d2c216f8375e9394163fa9d78f
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-12-25 00:58:12 +0200

    migrate to new implementation based on the documentation

--- app/src/main/java/com/nicos/ink_api_compose/presentation/drawing_screen/DrawingScreen.kt
+++ app/src/main/java/com/nicos/ink_api_compose/presentation/drawing_screen/DrawingScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableIntState
