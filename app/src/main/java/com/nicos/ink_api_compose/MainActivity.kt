cf6d7574c7fd41ed64655d218a9d7c52a0cc06d1
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-12-25 23:36:11 +0200

    UI updates

--- app/src/main/java/com/nicos/ink_api_compose/MainActivity.kt
+++ app/src/main/java/com/nicos/ink_api_compose/MainActivity.kt
@@ -7,8 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.ink.geometry.ImmutableBox
-import androidx.ink.geometry.Vec
 import com.nicos.ink_api_compose.presentation.drawing_screen.DrawingSurface
 import com.nicos.ink_api_compose.ui.theme.Ink_Api_ComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
