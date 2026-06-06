0db3d241bd3dba32f0681b7f450d51368109b520
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-06-07 01:27:31 +0300

    some bug fixes

--- app/src/main/java/com/nicos/ink_api_compose/presentation/drawing_screen/DrawingState.kt
+++ app/src/main/java/com/nicos/ink_api_compose/presentation/drawing_screen/DrawingState.kt
@@ -1,8 +1,6 @@
 package com.nicos.ink_api_compose.presentation.drawing_screen
 
 import android.graphics.Bitmap
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.ink.strokes.Stroke
 
 data class DrawingState(
