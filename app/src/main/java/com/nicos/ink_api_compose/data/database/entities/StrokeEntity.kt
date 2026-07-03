a007f6d16029e379c8de38f3b8c2f6a787db7514
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-07-04 01:05:42 +0300

    migrate to Room 3

--- app/src/main/java/com/nicos/ink_api_compose/data/database/entities/StrokeEntity.kt
+++ app/src/main/java/com/nicos/ink_api_compose/data/database/entities/StrokeEntity.kt
@@ -1,7 +1,7 @@
 package com.nicos.ink_api_compose.data.database.entities
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room3.Entity
+import androidx.room3.PrimaryKey
 import com.nicos.ink_api_compose.data.stroke_converter.SerializedStockBrush
 
 @Entity
