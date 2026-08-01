72f01cd8b2e3d30be899785b231d05f860d0199d
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-08-02 01:33:31 +0300

    new refresh design

--- app/src/main/java/com/nicos/ink_api_compose/presentation/drawing_screen/DrawingScreen.kt
+++ app/src/main/java/com/nicos/ink_api_compose/presentation/drawing_screen/DrawingScreen.kt
@@ -9,14 +9,13 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
